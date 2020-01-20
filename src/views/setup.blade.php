519177e3fa0c3314172f154bbb03e8bbf72129ec
Abdul <abdul@uitoux.in>
2020-01-20 12:17:43 +0530

    Update setup.blade.php

--- src/views/setup.blade.php
+++ src/views/setup.blade.php
@@ -5,6 +5,7 @@
 @endif
 
 <script type="text/javascript">
+	//CRON JOBS
     var cron_job_list_template_url = "{{URL::asset($cron_job_pkg_prefix.'/public/angular/cron-job-pkg/pages/cron-job/list.html')}}";
     var cron_job_form_template_url = "{{URL::asset($cron_job_pkg_prefix.'/public/angular/cron-job-pkg/pages/cron-job/form.html')}}";
     var cron_job_view_template_url = "{{URL::asset($cron_job_pkg_prefix.'/public/angular/cron-job-pkg/pages/cron-job/view.html')}}";
@@ -12,5 +13,8 @@
     var cron_job_delete_data_url = "{{url('cron-job-pkg/cron-job/delete/')}}";
     var cron_job_view_data_url = "{{url('cron-job-pkg/cron-job/view/')}}";
     var cron_job_filter_data_url = "{{route('getCronJobFilter')}}";
+
+	//CRON JOB REPORTS
+
 </script>
 <script type="text/javascript" src="{{URL::asset($cron_job_pkg_prefix.'/public/angular/cron-job-pkg/routes-and-components.js')}}"></script>
