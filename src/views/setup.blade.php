36da2e7647b8e776d8ea39635f65a11d6a05f990
Abdul <abdul@uitoux.in>
2020-02-27 12:03:03 +0530

    u

--- src/views/setup.blade.php
+++ src/views/setup.blade.php
@@ -1,10 +1,39 @@
-@if(config('custom.PKG_DEV'))
+@if(config('cron-job-pkg.DEV'))
     <?php $cron_job_pkg_prefix = '/packages/abs/cron-job-pkg/src';?>
 @else
     <?php $cron_job_pkg_prefix = '';?>
 @endif
 
 <script type="text/javascript">
+
+    app.config(['$routeProvider', function($routeProvider) {
+
+        $routeProvider.
+        //CRON JOB
+        when('/cron-job-pkg/cron-job/list', {
+            template: '<cron-job-list></cron-job-list>',
+            title: 'Cron Jobs',
+        }).
+        when('/cron-job-pkg/cron-job/add', {
+            template: '<cron-job-form></cron-job-form>',
+            title: 'Add Cron Job',
+        }).
+        when('/cron-job-pkg/cron-job/edit/:id', {
+            template: '<cron-job-form></cron-job-form>',
+            title: 'Edit Cron Job',
+        }).
+        when('/cron-job-pkg/cron-job/view/:id', {
+            template: '<cron-job-view></cron-job-view>',
+            title: 'View Cron Job',
+        }).
+
+        //CRON JOB REPORT
+        when('/cron-job-pkg/cron-job-report/list', {
+            template: '<cron-job-report-list></cron-job-report-list>',
+            title: 'Cron Job Reports',
+        });
+    }]);
+
 	//CRON JOBS
     var cron_job_list_template_url = "{{URL::asset($cron_job_pkg_prefix.'/public/angular/cron-job-pkg/pages/cron-job/list.html')}}";
     var cron_job_form_template_url = "{{URL::asset($cron_job_pkg_prefix.'/public/angular/cron-job-pkg/pages/cron-job/form.html')}}";
